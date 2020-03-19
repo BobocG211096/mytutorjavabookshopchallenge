8c2f0e05b6ce9665d74e9acaa9e2b0adc90be100
Boboc Costin-Gabriel <boboc.g21@gmail.com>
2020-03-19 09:48:43 +0000

    Create the Spring Boot application class runner.

---
+++ src/main/java/co/uk/mytutor/BookshopApplication.java
@@ -0,0 +1,11 @@
+package co.uk.mytutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookshopApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BookshopApplication.class);
+    }
+}
